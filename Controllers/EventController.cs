ef583ad0809f46f6100f818871b88fa4e654ec7c
Mussab12 <shahidmussab7@gmail.com>
2023-03-08 18:17:25 +0500

    UI adjusted

---
+++ Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginSystem.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult EventPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using LoginSystem.Models.Domain;
+using System.Runtime.InteropServices;
+
+namespace LoginSystem.Models;
+
+    public class Event
+    {
+    public int? Eventid { get; set; }
+    [Required]
+    public string?Subject { get; set; }
+    [Required]
+    public string? StartDate { get; set; }
+    public string? EndDate { get; set; }
+    public string? Description { get; set; }
+
+    public string? MyApplicationUserId { get; set; } // Add this property
+    public ApplicationUser? ApplicationUser { get; set; } ///Foreign key
+}
+
